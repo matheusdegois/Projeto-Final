128e098fc6d1b44d427650cef70522c3861e9b8a
matheusdegois <163362900+matheusdegois@users.noreply.github.com>
2025-03-06 20:07:53 -0300

    1.1.7

--- index.php
+++ index.php
@@ -6,21 +6,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CEEP</title>
     <link rel="stylesheet" href="style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/@fancyapps/ui@5.0/dist/fancybox/fancybox.css"/>
 </head>
 
 <body>
-
     <div class="container fundo">
         <div class="row borda">
-            <div class="col-4 fundologo">
-                <img src="CEEP1.png" alt="" class="d-block w-100">
-
+            <div class="col-4">
+                <img src="CEEP1.png" alt="" class="d-block w-100 logo">
             </div>
             <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="#">NAIARA BABARESCO</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <a class="navbar-brand" href="#">NAIARA BABARESCO DE SOUZA</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarNav">
@@ -31,42 +34,45 @@
                             <li class="nav-item">
                                 <a class="nav-link" href="cursos.php">Cursos Disponíveis</a>
                             </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="institucional.php">Institucional</a>
+                            </li>
                         </ul>
                     </div>
                 </div>
             </nav>
         </div>
     </div>
-    <div class="container">
-        <div class="row borda">
-            <div class="col-12 fundoImagem">
-                <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-                    <div class="carousel-inner">
-                        <div class="carousel-item active">
-                            <img src="imagemalunos.webp" class="d-block w-100 Imagem" alt="...">
-                        </div>
-                        <div class="carousel-item">
-                            <img src="imagemalunos.webp" class="d-block w-100 Imagem" alt="...">
-                        </div>
-                        <div class="carousel-item">
-                            <img src="imagemalunos.webp" class="d-block w-100 Imagem" alt="...">
-                        </div>
-                    </div>
-                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Anterior</span>
-                    </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Proximo</span>
-                    </button>
-                </div>
+    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="imagemalunos.webp" class="d-block w-100 Imagem" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="imagemalunos.webp" class="d-block w-100 Imagem" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="imagemalunos.webp" class="d-block w-100 Imagem" alt="...">
             </div>
         </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
+            data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Anterior</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"
+            data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Proximo</span>
+        </button>
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
-    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+        </script>
+    <script src="https://cdn.jsdelivr.net/npm/@fancyapps/ui@5.0/dist/fancybox/fancybox.umd.js"></script>
 </body>
+
+
 
 </html>
---
+++ institucional.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fancyapps/ui@5.0/dist/fancybox/fancybox.css" />
+</head>
+
+<body>
+    <div class="container fundo">
+        <div class="row borda">
+            <div class="col-4">
+                <img src="CEEP1.png" alt="" class="d-block w-100 logo">
+            </div>
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">NAIARA BABARESCO DE SOUZA</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav ms-auto">
+                            <li class="nav-item">
+                                <a class="nav-link active" href="index.php">Início</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="cursos.php">Cursos Disponíveis</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="institucional.php">Institucional</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row borda">
+            <div class="col-6 text-center">
+                <h3>Bem vindo ao colégio NAIARA BAVARESCO DE SOUZA</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut convallis vel metus euismod vulputate.
+                    Nunc ornare ex id metus viverra efficitur. Vestibulum lacinia sem non ullamcorper mollis. Nulla
+                    fermentum tellus mi, a molestie erat semper suscipit. Pellentesque ullamcorper velit elit, placerat
+                    faucibus lorem congue eu. Aliquam sit amet dignissim tellus. Ut nec erat quis turpis feugiat
+                    condimentum eu sit amet lectus. Praesent cursus porttitor sapien, ut consequat nulla sollicitudin
+                    in. Nulla dignissim nunc ut diam faucibus euismod eget ut velit. Nunc feugiat ornare diam, in
+                    interdum ex pharetra ac. Cras eu finibus eros. Aliquam convallis dictum felis, eget iaculis libero
+                    tempus vel.
+
+                    Vestibulum turpis velit, malesuada sit amet dapibus cursus, vulputate ac sem. Curabitur risus diam,
+                    eleifend a efficitur nec, efficitur sed nisl. Sed ornare accumsan dictum. Sed enim elit, faucibus
+                    eget nibh condimentum, ullamcorper efficitur elit. Aenean lectus leo, rutrum vel ornare vitae,
+                    hendrerit eu erat. Vestibulum urna elit, euismod ut velit nec, congue aliquam odio. Curabitur
+                    aliquam et neque non convallis. Curabitur ut velit eros. Aenean rutrum dolor dui, a facilisis nisl
+                    ullamcorper at. Maecenas eu aliquet nisl. Praesent lobortis sem eros, sed interdum nibh eleifend id.
+                    Cras orci mi, lobortis congue mattis in, eleifend sed odio.
+
+                    Sed cursus, justo vel malesuada porta, ligula nisi commodo mi, et faucibus massa libero luctus mi.
+                    Nam magna ipsum, fringilla ac consequat eget, eleifend ac nulla. Vivamus sed ligula posuere,
+                    scelerisque elit sit amet, hendrerit massa. Etiam iaculis mollis fermentum. Nam condimentum, nunc
+                    eget suscipit vulputate, augue felis laoreet neque, eu dignissim eros velit eu nisi. Duis id neque
+                    at lectus facilisis rutrum. Sed dolor sapien, aliquam in iaculis vel, elementum nec massa.
+
+                    Pellentesque eget odio semper, auctor quam eu, lobortis urna. Cras at lacus id diam tincidunt
+                    venenatis mollis ut nunc. Pellentesque suscipit porttitor pellentesque. Maecenas tincidunt est in
+                    libero bibendum volutpat. Phasellus sodales, lacus eget posuere ultrices, orci neque bibendum dui,
+                    nec tempus enim sem nec tortor. Donec fermentum diam diam, ut viverra nulla malesuada et. Nulla id
+                    lobortis nisi. Duis quis nisi mi. In hac habitasse platea dictumst. Aenean purus ligula, vehicula
+                    fermentum ultrices a, vestibulum at neque. Nullam in lacus nec orci vehicula pretium. Proin eu
+                    tempus libero.
+
+                    Sed iaculis nec arcu sed eleifend. Cras lacinia dui libero, nec condimentum felis porttitor ac.
+                    Donec in mauris ac dui auctor eleifend. In hac habitasse platea dictumst. Nulla vehicula sed mauris
+                    euismod aliquet. Cras faucibus sapien id condimentum ultrices. Donec et porttitor dui.
+                </p>
+               
+
+            </div>
+            <div class="col-6 text-center">
+                   <img src="" alt=""> 
+                    </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+        </script>
+    <script src="https://cdn.jsdelivr.net/npm/@fancyapps/ui@5.0/dist/fancybox/fancybox.umd.js"></script>
+</body>
+
+</html>

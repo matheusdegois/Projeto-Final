43eab05c5770add5fb88aaf6b31e7a49a044bbe8
matheusdegois <163362900+matheusdegois@users.noreply.github.com>
2025-03-13 22:10:10 -0300

    1.2.4

--- menu.php
+++ menu.php
@@ -1,17 +1,4 @@
 <!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CURSOS</title>
-    <link rel="stylesheet" href="style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fancyapps/ui@5.0/dist/fancybox/fancybox.css" />
-</head>
-
-<body>
 <div class="row bg-success bg-gradient">
         <div class="col-3">
             <a href="index.php">
@@ -29,7 +16,7 @@
     </div>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">NAIARA BABARESCO DE SOUZA</a>
+            <a class="navbar-brand" href="index.php">NAIARA BABARESCO DE SOUZA</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -51,12 +38,10 @@
                     <li class="nav-item">
                         <a class="nav-link" href="galeria.php">Galeria</a>
                     </li>
-
                 </ul>
-                <hr>
             </div>
         </div>
     </nav>
+    <hr>
   
-    </body>
     </html>

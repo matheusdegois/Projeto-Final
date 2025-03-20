a09cec91d2ec6416c7ba81e5538250269e5cd194
matheusdegois <163362900+matheusdegois@users.noreply.github.com>
2025-03-19 22:08:28 -0300

    2.0.3

--- menu.php
+++ menu.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 
 <body>
@@ -14,12 +15,11 @@
             </a>
         </div>
         <div class="col-6 text-center align-items-center d-flex flex-column justify-content-center">
-            <h1 style="font-size: 1.5rem;">Colégio especializado em </h1>
-            <h1 style="font-size: 1.75rem;">formação técnica e média</h1>
+            <h1 style="font-size: 1.5rem;">COLÉGIO TECNICO</h1>
+            <h1 style="font-size: 1.75rem;">NAIANA BABARESCO DE SOUZA</h1>
         </div>
         <div class="col-3">
             <img src="paranabrasao.png" alt="" class="w-50 rounded float-right">
-
         </div>
     </div>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark menu-fixo">

b9b2e14e225db17a76522361a2d090f5b40c8a8d
matheusdegois <163362900+matheusdegois@users.noreply.github.com>
2025-03-20 21:26:33 -0300

    Cursos arrumados

--- menu.php
+++ menu.php
@@ -1,36 +1,40 @@
 <!DOCTYPE html>
+<html lang="pt-BR">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Meu Site</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="menu.css">
 </head>
 
 <body>
-    <div class="row bg-success bg-gradient menu-fixo">
-        <div class="col-3">
-            <a href="index.php">
-                <img src="ceep2.png" alt="" class="d-block w-50">
-            </a>
+
+
+    <header class="banner">
+        <div class="row">
+            <div class="ceep col-2">
+                <a href="index.php">
+                    <img src="ceep2.png" alt="Logo CEEP">
+                </a>
+            </div>
+            <div class="col-6 text-center align-items-center d-flex flex-column justify-content-center">
+                <h1 id="titulo">Colégio especializado em </h1>
+                <h1>formação técnica e média</h1>
+            </div>
         </div>
-        <div class="col-6 text-center align-items-center d-flex flex-column justify-content-center">
-            <h1 style="font-size: 1.5rem;">COLÉGIO TECNICO</h1>
-            <h1 style="font-size: 1.75rem;">NAIANA BABARESCO DE SOUZA</h1>
-        </div>
-        <div class="col-3">
-            <img src="paranabrasao.png" alt="" class="w-50 rounded float-right">
-        </div>
-    </div>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark menu-fixo">
+    </header>
+
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="index.php">NAIANA BABARESCO DE SOUZA</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ms-auto">
+                <ul class="navbar-nav">
                     <li class="nav-item">
                         <a class="nav-link active" href="index.php">Início</a>
                     </li>
@@ -43,8 +47,8 @@
                             Cursos Disponíveis
                         </a>
                         <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="cursossubsequente.php">Subsequente</a></li>
-                            <li><a class="dropdown-item" href="cursosintegrado.php">Integrado</a></li>
+                            <li><a class="dropdown-item" href="subsequente.php">Subsequente</a></li>
+                            <li><a class="dropdown-item" href="integrado.php">Integrado</a></li>
                         </ul>
                     </li>
                     <li class="nav-item">
@@ -57,6 +61,10 @@
             </div>
         </div>
     </nav>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>

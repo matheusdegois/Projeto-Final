bfb96c526f03be7cb22e59bb6da0a840e0c66c30
matheusdegois <163362900+matheusdegois@users.noreply.github.com>
2025-03-28 19:08:18 -0300

    Update menu.php
    Co-Authored-By: Felipe0020 <166182089+Felipe0020@users.noreply.github.com>

--- menu.php
+++ menu.php
@@ -39,7 +39,7 @@
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link active" href="index.php">Início</a>
+                        <a class="nav-link " href="index.php">Início</a>
                     </li>
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
